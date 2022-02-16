54976b3b3770631c5f258f29d8685f0509c7428b
havardsollie <79274753+havardsollie@users.noreply.github.com>
2022-02-16 19:04:50 +0100

    filter

--- js/data/products.js
+++ js/data/products.js
@@ -1,19 +1,10 @@
 const url = "https://fakestoreapi.com/products";
+const productContainer = document.querySelector(".products"); 
 
-async function getData () {
+
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    productContainer.innerHTML = "";
 
-    return data;
-}
 
-getData();
-
-export default function createHTML(data) {
-    return `<div class="display">
-                <h2>${data[0].title}>
-                <h3>${data[0].price}>
-            <div>`
-
-};
+export default data;
--- js/index.js
+++ js/index.js
@@ -1,7 +1,35 @@
-import createHTML from "./data/products.js";
-console.log(createHTML);
+import data from "./data/products.js";
 
 const productContainer = document.querySelector(".products");
+const filter = document.querySelector(".search");
 
+let productLoop = data;
 
-productContainer.innerHTML += createHTML;
+function renderProducts () {
+    productContainer.innerHTML = "";
+
+    productLoop.forEach(function (display) {
+        productContainer.innerHTML +=  `<div class="display">
+                    <h2>${display.title}</h2>
+                    <h3>${display.price}</h3>
+                <div>`
+    
+    });
+};
+
+renderProducts();
+
+filter.onkeyup = function (event) {
+
+    const filterValue = event.target.value.trim().toLowerCase();
+
+    const filteredProducts = data.filter(function (display) {
+        if (display.title.toLowerCase().startsWith(filterValue)) {
+            return true;
+        }
+    });
+
+    productLoop = filteredProducts;
+
+    renderProducts();
+};
