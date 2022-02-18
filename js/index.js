01aeb60acb1878fdd2e2f702b80bd9b52ad45c68
havardsollie <79274753+havardsollie@users.noreply.github.com>
2022-02-18 09:38:20 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 
 import data from "./data/products.js";
-console.log(data);
+
 
 const productContainer = document.querySelector(".products");
 const button = document.querySelector(".btn");
 
 let productLoop = data;
+
 
 function renderProducts () {
     productContainer.innerHTML = "";
@@ -14,18 +15,38 @@ function renderProducts () {
         productContainer.innerHTML +=  `<div class="display">
                     <h2>${data.title}</h2>
                     <h3>$${data.price}</h3>
-                <div>`  
+                <div>`
+                
     });
-
-    let prices = data.price;
-
-    button.onclick = function () {
-
-        prices.sort(function (a,b,) {
-            return (a - b);
-        })
-    }; 
 };
 
 renderProducts();
+
+var maxPrices = productLoop.filter(function(data) {
+    return data.price < 50;
+})
+
+console.log(maxPrices);
+
+
+// maxPrices.forEach(function(data) {
+//     button.onclick = function() {
+//         maxPrices.sort(function(a,b) {
+//             return a - b;
+//         })
+//     }
+// })
+
+// console.log(maxPrices);
+
+// button.onclick = (function (maxPrices) {
+
+//     maxPrices.filter(function(a,b) {
+//         return a - b;
+//         console.log(a);
+//     });
+// });
+
+// renderProducts(maxPrices);
+
 
